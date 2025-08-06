77480670241bab83648d1c6d9542ebb5e5fb9376
nakulupadhyay <142798383+nakulupadhyay@users.noreply.github.com>
2025-08-06 08:18:50 +0530

    Create palindromechecker.java

---
+++ work/palindromechecker.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String input = scanner.nextLine().toLowerCase();
+
+        if (isPalindrome(input)) {
+            System.out.println(" It is a palindrome.");
+        } else {
+            System.out.println("Not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
